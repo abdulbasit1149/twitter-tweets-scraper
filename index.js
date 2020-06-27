b575f73f15c3d1fb754d4c278b9774b083f93805
Abdul Basit <brcoopbasit@gmail.com>
2020-06-27 11:02:28 -0400

    initial files

---
+++ index.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer');
+
+console.log("Twitter Scraper");
+
+(async () => {
+    const browser = await puppeteer.launch(
+        {args: ['--no-sandbox', '--disable-setuid-sandbox']}
+    );
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com/');
+    await page.screenshot({ path: 'google.png' });
+
+    await browser.close();
+})()
